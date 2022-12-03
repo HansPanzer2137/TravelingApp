2872d5207850464cacdc8ab72b87a596c9f6a7ee
HansPanzer2137 <htopolski12@gmail.com>
2022-12-03 15:33:41 +0100

    Initial commit of algorithm,trying to connect calculations with layout, basic View was added. TODO: graph editing and Mpas SDK integration

---
+++ app/src/main/java/com/example/travelingapp/Graph.kt
@@ -0,0 +1,48 @@
+package com.example.travelingapp
+
+import android.util.Log
+import kotlin.random.Random
+
+data class Edge(val n1:Int,val n2: Int, val c:Int)
+data class Vertex(val n: Int, val c:Int)
+
+class Graph(val nodeCount: Int,val edgeCount: Int) {
+    private lateinit var adjacencyList: List<List<Vertex>>
+    lateinit var edges: List<Edge>
+
+    fun getEdge(n1:Int,n2:Int): Vertex? {
+        return if (n1<n2){
+            adjacencyList[n1].find { it.n == n2 }
+        }else {
+            adjacencyList[n2].find { it.n == n1 }
+        }
+    }
+
+    constructor(nodeCount: Int = 8, edgeCount: Int = 50, maxEdgeCost: Int = 10) : this(nodeCount,edgeCount) {
+
+        val mutableAL : List<MutableList<Vertex>> = List(nodeCount) { mutableListOf() }
+        val mutableEdges = MutableList(edgeCount) { Edge(0, 0, 0) }
+
+        for(i in 0 until edgeCount){
+
+            val n1 = Random.nextInt(nodeCount)
+            val r = Random.nextInt(nodeCount - 1)
+
+            val c = Random.nextInt(1,maxEdgeCost)
+
+            // assure that the 2 random numbers are not the same
+            // always saving edges that lead smaller to bigger
+            if (n1 > r){
+                mutableAL[r].add(Vertex(n1,c))
+            } else {
+                mutableAL[n1].add(Vertex(r+1,c))
+            }
+
+        }
+
+        adjacencyList = mutableAL
+        edges = mutableEdges
+    }
+
+
+}
--- app/src/main/java/com/example/travelingapp/MainActivity.kt
+++ app/src/main/java/com/example/travelingapp/MainActivity.kt
@@ -1,11 +1,97 @@
 package com.example.travelingapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    class Salesman{
+        public var logcat = ""
+        private fun <T>permutations(list: List<T>)  = sequence<List<T>> {
+            val factorials = IntArray(list.size + 1)
+            factorials[0] = 1
+            for (i in 1..list.size) {
+                factorials[i] = factorials[i - 1] * i
+            }
+            for (i in 0 until factorials[list.size]) {
+                var onePermutation = mutableListOf<T>()
+                var temp = list
+                var positionCode = i
+                for (position in list.size downTo 1) {
+                    val selected = positionCode / factorials[position - 1]
+                    onePermutation += temp[selected]
+                    positionCode %= factorials[position - 1]
+                    temp = list.subList(0, selected) + temp.subList(selected + 1,temp.size);
+                }
+                yield(onePermutation)
+            }
+        }
+        var graph:Graph;
+
+        constructor(graph: Graph){
+            this.graph = graph
+        }
+
+        fun calculatePath(permutation: List<Int>): Int? {
+
+            var currentPath = 0
+            for (i in 1 until permutation.size){
+
+                val n1 = permutation[i-1]
+                val n2 = permutation[i]
+
+                val edge = graph.getEdge(n1,n2) ?: return null
+
+                currentPath +=  edge.c
+
+            }
+            return currentPath
+        }
+
+        fun solve(): Pair<List<Int>, Int>? {
+            val permutations = permutations((0 until graph.nodeCount).toList() )
+
+            var ans = Pair(listOf<Int>(),Int.MAX_VALUE)
+
+            for (permutation in permutations){
+                Log.d("TRY",permutation.toString())
+                logcat = logcat + permutation.toString() + "\n"
+
+
+                var currentPath = calculatePath(permutation) ?: continue
+                if (currentPath <  ans.second) {
+                    ans = Pair(permutation,currentPath)
+                }
+            }
+            if (ans.second == Int.MAX_VALUE){
+                return null
+            }
+            return ans
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val calcBNT: Button = findViewById(R.id.calculate)
+        val logTXT: TextView = findViewById(R.id.logText)
+        val finalTXT: TextView = findViewById(R.id.text2137)
+
+
+        calcBNT.setOnClickListener{
+            var graph = Graph()
+            var algo = Salesman(graph)
+            var path = algo.solve()
+            logTXT.text = algo.logcat
+            Log.d("ans",path.toString())
+            finalTXT.text = path.toString()
+        }
+
     }
 }
